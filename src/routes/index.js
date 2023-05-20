9363b6460bc0b7d8eed3d2a283bdf120ece19f74
austin <villasanamallariaustin@gmail.com>
2023-05-21 02:38:10 +0800

    temp test 15

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 // const tracerFormRoutes = require('./tracerFormRoutes');
 const authenticationRoutes = require('./authenticationRoutes');
-// const jobsRoutes = require('./jobsRoutes');
+const jobsRoutes = require('./jobsRoutes');
 
 module.exports = {
     // tracerFormRoutes,
     authenticationRoutes,
-    // jobsRoutes,
+    jobsRoutes,
 }
